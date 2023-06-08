c07b0577e570af00d5a0788a56f15db2c139a1d5
Takeuchi <>
2023-06-08 22:07:39 +0900

    わすれてたやつ一応いれとく

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,10 @@ export default {
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div v-for="item in car">
+        <HelloWorld :msg="item" />
+      </div>
+     <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -57,6 +60,8 @@ export default {
 
   <RouterView />
   <button @click="isShow = !isShow">toggle</button> 
+
+  
 </template>
 
 <style scoped>
