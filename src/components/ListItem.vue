64c9c10131f4ba8b5e9f572ea0e600239e956390
Tns <>
2023-06-16 21:51:14 +0900

    道場 - コンポーネントの分割

---
+++ src/components/ListItem.vue
@@ -0,0 +1,18 @@
+<template>
+   <li>{{ msg }}  - <button>btn</button></li>
+</template>
+
+
+<script> 
+export default {
+    //  props（プロパティｓ）
+  props: {
+     msg: {
+      type: String,
+      default: "Hi"
+    }
+  }
+}
+
+
+</script>
--- src/pages/develop.vue
+++ src/pages/develop.vue
@@ -1,12 +1,23 @@
 <script>
+
+import ListItem from '@/components/ListItem.vue'
 export default{
+    components: {
+        ListItem // この定義忘れるとこんそるで黄色に怒られる
+    },
     data(){
         return{
             key:[ 1, 5, 10, 12, 34],
             count: 1,
             tmpMessage: "sample",
             message: "",
-            messages : [ "test", "testtest"]
+            messages : [ "test", "testtest"],
+            arr: [ 1000, 3, 5, 'I am a cat.', 'You are a cat.'],
+            car :{
+                name: "kuruma",
+                color: "akai",
+                speed: "hayai",
+            },
         }
         
     },
@@ -41,52 +52,13 @@ export default{
     <div>
         <h1>Develop……つまり砂場にゃ！</h1>
 
-    <div>
-        {{message}} 
-
-    </div>
-        <ul>
-            <li v-for="messages in messages">{{messages}}ですわ。</li>
-        </ul>
-        <input v-model="tmpMessage" />
-        <button @click="addMessage">addMessage</button>
-        <button @click="delMessage">でる！！</button>
-
-<hr>
-
-        <h2>ねこちゃんを増やす</h2>
-        <div>{{ count }}</div>
-        <div>繰り返しながらボタンを作ってみよう</div>
-        <button v-for="key in key" @click="add(key)">{{key}}増やすボタン？</button>
-        <br><br>
-<hr>
-        <h2>いべんとあれこれ</h2>
         <div>
-            こんそるにログ出せって言ってる！こんそるみるべし！<br>
-        <p>@click="sample" は @click="sample($event)"という意味。</p>
-        <p>$eventはVueにおける予約語です。</p>
-        <button @click="sample">@click="sample"</button><br>
-        
-        <button @click="sample()">@click="sample()"</button><br>
-        <p>引数がひとつもないと、 undefiendなる</p>
-        <button @click="sample(1)">@click="sample(1)"</button><br>
-        <p>これは1が出るはず</p>
-        
-<hr>
-
-        <div>だがこれはどうかな？</div>
-        <p>add(num)を使ってみる</p>
-        <button @click="add(1)">めそっどのたすいち</button>
-
-        <p>普通のボタン</p>
-        <button @click="increment">たす1</button>
-        <button @click="decrement">まいなす</button>
-        <button @click="count = count +2">たすにをしてみる</button>
-
-        <br><br><br>
-    </div>
-
-
+        <ul>
+        <ListItem :msg="item" v-for="(item, key) in car" :key="key"></ListItem>
+        </ul>
+        </div>
+        <p>msg="item"だと文字列</p>
+        <p>:msg="  このなかはJavaScript "</p>
 
     </div>
 </template>
