7510863038dd82baf18fb1a570831d511b4dafcf
Tns <>
2023-07-25 19:48:51 +0900

    ボタンを押すとランダムでコンソールに数字が出る

--- src/pages/gacha.vue
+++ src/pages/gacha.vue
@@ -1,3 +1,20 @@
+<script>
+export default{
+    data() {
+        return{
+            result: 0
+        }
+    }, 
+    methods: {
+        gacha(){
+            this.result = Math.random()
+            console.log(this.result)
+        }
+    }
+}
+</script>
+
 <template>
     <div>がちゃをおくばしょ</div>
+    <v-btn @click="gacha">ガチャを引く！</v-btn>
 </template>
