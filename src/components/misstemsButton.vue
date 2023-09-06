4eb5cf1dbfe080ef3075b260d2439ef3ea507da2
Tns <>
2023-09-07 02:14:14 +0900

    みすてむずてむずボタンの作成

---
+++ src/components/misstemsButton.vue
@@ -0,0 +1,31 @@
+<script>
+export default{
+    data(){
+        return {
+            misstemsStr:"みす",
+            buttonStrings:["てむず", "みすきー", "しす"]
+        }
+    },
+    methods: {
+        addStr(){
+            this.misstemsStr += 'てむず'
+            console.log(this.misstemsStr)
+        }
+    }
+}
+</script>
+<template>
+    <v-container>
+        {{ misstemsStr }}
+        <v-spacer></v-spacer>
+        <v-btn @click="addStr">てむず</v-btn>
+        
+        <!-- 作りかけ
+
+        <div v-for="item in buttonStrings">
+            <v-btn @click="misstemsStr(item)">{{ item }}</v-btn>
+        </div>
+    
+        -->
+    </v-container>
+</template>
